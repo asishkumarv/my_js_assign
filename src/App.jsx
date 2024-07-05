3760a493dbb1be75ba6efc2e59974b59eb86b5d6
asishkumarv <asishkumarv@outlook.com>
2024-07-05 20:58:24 +0530

    footerlinks changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "ASISH VARANASI",
+  title: "Web Designer & Developer",
+  email: "asishkumarv@gmail.com",
+  gitHub: "asishkumarv",
+  instagram: "ahikmrsu",
+  linkedIn: "asish-kumar-varanasi-3b2942214",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "@asishvaranasi",
 };
 
 const primaryColor = "#4E567E";
